0c6081f9b93979a1966631f31a213af953b90ce6
pomidorka2010 <lamc560ki@hotmail.com>
2025-08-16 17:56:31 +0200

    Add comments for clarity and maintainability

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user');
 
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
+// Parse incoming JSON requests
 app.use(bodyParser.json());
 
+// Connect to MongoDB database
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
@@ -22,6 +25,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
+// Start the server on the specified port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
